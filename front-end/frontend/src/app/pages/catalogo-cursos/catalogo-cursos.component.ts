4c9de3adb82c9831ad3f971324f537e0020f253e
Jullyo1122 <jullyoc74@gmail.com>
2025-11-01 20:03:12 -0300

    Pagina de catologo criada

---
+++ front-end/frontend/src/app/pages/catalogo-cursos/catalogo-cursos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo-cursos',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalogo-cursos.component.html',
+  styleUrl: './catalogo-cursos.component.css'
+})
+export class CatalogoCursosComponent {
+
+}
