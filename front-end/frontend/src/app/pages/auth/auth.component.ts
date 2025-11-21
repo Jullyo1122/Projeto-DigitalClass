32fbf9c8c432e6dcb6f28dd70fbfdd44460c5785
Jullyo1122 <jullyoc74@gmail.com>
2025-11-21 20:45:33 -0300

    manutenção para pagina auth funcionar realizada

--- front-end/frontend/src/app/pages/auth/auth.component.ts
+++ front-end/frontend/src/app/pages/auth/auth.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -11,34 +10,5 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
-  id!: number;
-  senha!: string;
-  role: string = "aluno"; // valor padrão
-  mensagem: any;
-
-  constructor(private authService: AuthService) {}
-
-  fazerLogin() {
-    const dados = {
-      id: this.id,
-      senha: this.senha,
-      role: this.role
-    };
-
-    this.authService.login(dados).subscribe({
-      next: (res) => {
-        this.mensagem = res.mensagem;
-
-        if (res.status === "sucesso") {
-          console.log("Logado como:", res.role);
-          // Aqui você pode redirecionar conforme role:
-          // if (res.role === "aluno") this.router.navigate(['/aluno']);
-          // if (res.role === "professor") this.router.navigate(['/prof']);
-        }
-      },
-      error: () => {
-        this.mensagem = "Erro no servidor!";
-      }
-    });
-  }
+  
 }
